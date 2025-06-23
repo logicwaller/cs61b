c9f3624c7bc37d9a8c7e00e043f6341fa7511866
logicwaller <a1370124587@163.com>
2025-06-23 09:38:34 +0800

    add Planet class

---
+++ proj0/Planet.java
@@ -0,0 +1,23 @@
+public class Planet {
+    public double xxPos, yyPos, xxVel, yyVel, mass;
+    public String imgFileName;
+
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img){
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
+    }
+
+    public Planet(Planet p){
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+}
